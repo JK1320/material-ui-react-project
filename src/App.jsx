42301f94dae32584ce7502865f227dd059e28178
JK1320 <kaurjus1@gmail.com>
2022-02-18 11:26:06 +0000

    created theme.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Button, makeStyles } from "@material-ui/core";
+import { Person } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    color: "white",
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
+
 const App = () => {
-  return <div>Hello world!</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button
+        className={classes.button}
+        variant="contained"
+        color="primary"
+        startIcon={<Person />}
+      >
+        Material UI
+      </Button>
+    </div>
+  );
 };
 
 export default App;
