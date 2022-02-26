185deca46dac55f1344664b51560462eb527e46e
JK1320 <kaurjus1@gmail.com>
2022-02-26 23:52:42 +0000

    added form

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,24 +1,88 @@
-import { Fab, makeStyles, Tooltip } from "@material-ui/core";
+import { Container, Fab, FormControlLabel, FormLabel, makeStyles, MenuItem, Modal, Radio, RadioGroup, TextField, Tooltip } from "@material-ui/core";
 import { Add as AddIcon } from "@material-ui/icons";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
     fab: {
         position: "fixed",
-        bottom: 10,
-        right: 10,
-    }
+        bottom: 20,
+        right: 20,
+    },
+    container: {
+        width: 500,
+        height: 550,
+        backgroundColor: "white",
+        position: "absolute",
+        margin: "auto",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        [theme.breakpoints.down("sm")]: {
+            width: "100vh",
+            height: "100vh",
+        }
+    },
 }));
 
 const Add = () => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [visibility, setVisibility] = useState("");
+
+  const handleChange = (event)=> {
+      setVisibility(event.target.value);
+  };
 
   return (
     <>
-    <Tooltip title="Add" aria-label="add">
+      <Tooltip title="Add" aria-label="add" onClick={() => setOpen(true)}>
         <Fab color="primary" className={classes.fab}>
-            <AddIcon />
+          <AddIcon />
         </Fab>
-    </Tooltip>
+      </Tooltip>
+      <Modal open={open}>
+        <Container className={classes.container}>
+          <form className={classes.form} autoComplete="off">
+            <div className={classes.item}>
+              <TextField
+                id="standard-basic"
+                label="Title"
+                size="small"
+                style={{ width: "100%" }}
+              ></TextField>
+            </div>
+            <div className={classes.item}>
+              <TextField
+                id="outlined-multiline-static"
+                multiline
+                rows={4}
+                defaultValue="Tell your story"
+                variant="outlined"
+                label="Description"
+                size="small"
+                style={{ width: "100%" }}
+              ></TextField>
+            </div>
+            <div className={classes.item}>
+              <TextField style={{ width: "50%" }} onChange={handleChange} select label="Visibility" value={visibility}>
+                <MenuItem value="Public">Public</MenuItem>
+                <MenuItem value="Private">Private</MenuItem>
+                <MenuItem value="Unlisted">Unlisted</MenuItem>
+              </TextField>
+            </div>
+            <div className={classes.item}>
+                <FormLabel component="legend">Who can comment?</FormLabel>
+               <RadioGroup>
+                   <FormControlLabel value="Everybody" control={<Radio />} label="Everybody" />
+                   <FormControlLabel value="My Friends" control={<Radio />} label="My Friends" />
+                   <FormControlLabel value="Nobody" control={<Radio />} label="Nobody" />
+                   <FormControlLabel disabled value="Custom" control={<Radio />} label="Custom (Premium)" />
+               </RadioGroup>
+            </div>
+          </form>
+        </Container>
+      </Modal>
     </>
   );
 };
